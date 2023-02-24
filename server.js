72dc8398b4789733327314588c6e06767542eef8
Thijmen <yeaaccount@gmail.com>
2023-02-25 00:52:23 +0100

    added extra routes changed configs

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
+const bodyParser = require('body-parser');
 
 
+app.use(fileUpload());
 
-
-
+const DB_URL = 'mongodb://127.0.0.1:27017/zenox';
 
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
-///console log stuff about the database
-const db = mongoose.connection
 
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log('Failed to connect to MongoDB', err);
+});
+
+const db = mongoose.connection;
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+app.use(express.json({limit: '200mb'}));
+app.use(express.text({ limit: '200mb' }));
 app.use(cookieParser())
-app.use(session({secret: "twethwethbternhj"}))
-
+app.use(session({ secret: "twethwethbternhj" }))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -34,18 +44,40 @@ app.use((req, res, next) => {
 });
 
 
+// fixing "413 Request Entity Too Large" errors
+
+
+// file upload api
+
+const User = require('./models/user');
+
 
 // Routes
 const authRoutes = require('./routes/auth');
 app.use('/', authRoutes);
 
+const profileRoutes = require('./routes/profile');
+app.use('/profile', profileRoutes);
+
+const messagesRoutes = require('./routes/messages');
+app.use('/messages', messagesRoutes);
+
+const devRoutes = require('./routes/dev');
+app.use('/dev', devRoutes);
+
 app.set('view engine', 'ejs')
-// require('./utilities/filelist')
 
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection:', err);
+  process.exit(1);
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
-})
-
-
+});
