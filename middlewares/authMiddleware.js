d0ebb1392d08e1a2014f55e59939ee9f3071128b
Thijmen <yeaaccount@gmail.com>
2023-02-26 01:09:20 +0100

    added auth middleware to check if the user accesing the endpoint is really the logged in user

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({token});
+
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user
+    next();
+  } catch (err) {
+    res.status(401).send({ error: 'Authentication required' });
+  }
+};
+
+
+module.exports = authMiddleware;
